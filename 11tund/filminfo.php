119f5b3fc16080cba72961c83fb6b18d56ec426f
“Ralf <ralfvihmaru@users.noreply.github.com>
2019-11-18 11:40:54 +0200

    Piltide hindamine ja avamine

---
+++ 11tund/filminfo.php
@@ -0,0 +1,27 @@
+<?php
+  require("../../../../../config_vp2019.php");
+  require("functions_film.php");
+  $userName = "Ralf Vihmaru";
+  $database = "if19_ralf_vi_1";
+  
+  
+	$filmInfoHTML = readAllFilms();
+	//lisame lehe päise
+	require("header.php");
+	
+?>
+<body>
+  <?php
+    echo "<h1>" .$userName ." koolitöö leht</h1>";
+  ?>
+  
+  <p>See leht on loodud koolis õppetöö raames ja ei sisalda tõsiseltvõetavat sisu!</p>
+  <hr>
+  <h2> Eesti filmid </h2>
+  <p> Praegu on andmebaasis järgmised filmid:</p>
+  <?php
+    //echo "server: " .$serverHost .", kasutaja: " .$serverUsername;
+	echo $filmInfoHTML;
+  ?>
+</body>
+</html>
